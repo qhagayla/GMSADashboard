671c5fccc48745df4307893ee4a6c2e3b6e46acc
qaflacsamana <142982685+qaflacsamana@users.noreply.github.com>
2024-01-10 17:12:44 +0800

    Updated Sidebar

--- client/src/Sidebar.jsx
+++ client/src/Sidebar.jsx
@@ -6,54 +6,58 @@ import {
   faSquarePollHorizontal,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
-import logoImage from "./components/Logo.png";
+
 import "./Sidebar.css";
 
 function Sidebar({ activeMenuItem, handleMenuItemClick }) {
+  const iconStyle = { color: 'white' };
+
   return (
     <div className="sidebar">
       <div className="sidebar-header">
-        <img src={logoImage} alt="Logo" className="logo-image" />
+        <h2>TINYTRACKS</h2>
       </div>
       <div className="menu">
         <ul>
+          {/* Menu items without the logo */}
           <li
             className={activeMenuItem === "dashboard" ? "active" : ""}
             onClick={() => handleMenuItemClick("dashboard")}
           >
-            <FontAwesomeIcon icon={faCalendar} size="lg" />{" "}
+            <FontAwesomeIcon icon={faCalendar} size="lg" style={iconStyle} />{" "}
             <span className="icon-text">Dashboard</span>
           </li>
           <li
             className={activeMenuItem === "recordedVideos" ? "active" : ""}
             onClick={() => handleMenuItemClick("recordedVideos")}
           >
-            <FontAwesomeIcon icon={faVideo} size="lg" />{" "}
+            <FontAwesomeIcon icon={faVideo} size="lg" style={iconStyle} />{" "}
             <span className="icon-text">Recorded Videos</span>
           </li>
           <li
             className={activeMenuItem === "results" ? "active" : ""}
             onClick={() => handleMenuItemClick("results")}
           >
-            <FontAwesomeIcon icon={faSquarePollHorizontal} size="lg" />{" "}
+            <FontAwesomeIcon icon={faSquarePollHorizontal} size="lg" style={iconStyle} />{" "}
             <span className="icon-text">Results</span>
           </li>
         </ul>
       </div>
       <div className="others">
         <ul>
+          {/* Other menu items without the logo */}
           <li
             className={activeMenuItem === "settings" ? "active" : ""}
             onClick={() => handleMenuItemClick("settings")}
           >
-            <FontAwesomeIcon icon={faGear} size="lg" />{" "}
+            <FontAwesomeIcon icon={faGear} size="lg" style={iconStyle} />{" "}
             <span className="icon-text">Settings</span>
           </li>
           <li
             className={activeMenuItem === "account" ? "active" : ""}
             onClick={() => handleMenuItemClick("account")}
           >
-            <FontAwesomeIcon icon={faUser} size="lg" />{" "}
+            <FontAwesomeIcon icon={faUser} size="lg" style={iconStyle} />{" "}
             <span className="icon-text">Account</span>
           </li>
         </ul>
